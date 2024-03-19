9612677d52af68edba8a918d9fa5c1baf325fa24
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-03-19 22:58:06 +0100

    modified:   index.html 	modified:   index.js added the class wrapper-state to the lightDark_mode click event 	modified:   style/style.css.map 	modified:   style/style.scss

--- index.js
+++ index.js
@@ -93,7 +93,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
-
     // changing mode(light and dark)
     lightDark_mode.addEventListener("click", () => {
         const body = document.body;
@@ -102,10 +101,12 @@ document.addEventListener("DOMContentLoaded", () => {
         // conditional statement of url("/images/bg-desktop-light.jpg") and url("/images/bg-mobile-light.jpg")
         if (wrapperImg.style.backgroundImage.includes('url("/images/bg-desktop-light.jpg")') || wrapperImg.style.backgroundImage.includes('url("/images/bg-mobile-light.jpg")')) {
             wrapperImg.style.backgroundImage = "url('/images/bg-desktop-dark.jpg')";
+
             //check screen width <= 380
             if (window.innerWidth <= 380) {
                 wrapperImg.style.backgroundImage = "url('/images/bg-mobile-dark.jpg')";
             }
+            
         } else {
             wrapperImg.style.backgroundImage = "url('/images/bg-desktop-light.jpg')";
             //check screen width <= 380
@@ -115,20 +116,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     
     
-    
+        const wrapperState = document.querySelector(".wrapper-state");
         if (lightDark_mode.src.includes("icon-moon.svg")) {
             lightDark_mode.src = "./images/icon-sun.svg";
 
+            wrapperState.style.backgroundColor = "hsl(235, 24%, 19%)";
             inputValue.style.backgroundColor = "hsl(235, 24%, 19%)";
             body.style.backgroundColor = 'hsl(235, 21%, 11%)'
             todoBody.style.backgroundColor = "hsl(235, 24%, 19%)"
         } else {
             lightDark_mode.src = "./images/icon-moon.svg"
 
+            wrapperState.style.backgroundColor = "hsl(236, 33%, 100%)";
             inputValue.style.backgroundColor = "hsl(236, 33%, 100%)";
             body.style.backgroundColor = 'hsl(0, 0%, 98%)'
             todoBody.style.backgroundColor = "hsl(236, 33%, 100%)"
         }
+
     })
     
 
